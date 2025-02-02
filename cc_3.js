d96ed44dbd575b8ad30bf4f0827dcfffa87c0c3a
Gavrie Grant <gavrie@usf.edu>
2025-02-02 18:08:10 -0500

    Task 2 - Modifying Customer Orders

--- cc_3.js
+++ cc_3.js
@@ -10,3 +10,10 @@ prices.shift();
 
 // Logged the array to the console
 console.log(prices); 
+
+// Task 2 Modifying Customer Orders Scenario
+// Decleared array of orders
+let orders = [10, 25, 15, 30, 20];
+orders[2] += 5;
+let total = orders.reduce((sum, order) => sum + order, 0);
+console.log(total);
