537c0b4f7a0c3ced3dc9dc7b66fac92d38c2b029
Gavrie Grant <gavrie@usf.edu>
2025-02-02 18:04:25 -0500

    Task 1 - Product Price Management

---
+++ cc_3.js
@@ -0,0 +1,12 @@
+// Task 1 Product Price Management Scenario
+// Decleared array of prices 
+let prices = [20, 50, 40, 70, 10];
+
+// Added 80 to the end of the array
+prices.push(80);
+
+// Removed the first element of the array
+prices.shift();
+
+// Logged the array to the console
+console.log(prices); 
